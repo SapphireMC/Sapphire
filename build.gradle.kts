7082e02264a3036e77ba7114d5593e6be3935442
DenaryDev <denaryplanet@gmail.com>
2022-05-02 01:19:24 +0500

    Build updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.5"
+    id("io.papermc.paperweight.patcher") version "1.3.6"
 }
 
 repositories {
@@ -16,8 +16,8 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.1:fat")
-    decompiler("net.minecraftforge:forgeflower:1.5.498.22")
+    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
+    decompiler("org.quiltmc:quiltflower:1.8.1")
     paperclip("io.papermc:paperclip:3.0.2")
 }
 
@@ -66,7 +66,7 @@ paperweight {
     serverProject.set(project(":sapphire-server"))
 
     remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+    decompileRepo.set("https://maven.quiltmc.org/repository/release/")
 
     usePaperUpstream(providers.gradleProperty("paperRef")) {
         withPaperPatcher {
