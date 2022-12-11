6b311740786036f61544a8e29cb88486053e42ca
DenaryDev <denaryplanet@gmail.com>
2022-12-11 21:06:45 +0500

    [ci skip] Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.9"
+    id("io.papermc.paperweight.patcher") version "1.3.11"
 }
 
 repositories {
@@ -19,7 +19,7 @@ repositories {
 
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
-    decompiler("org.quiltmc:quiltflower:1.8.1")
+    decompiler("org.quiltmc:quiltflower:1.9.0")
     paperclip("io.papermc:paperclip:3.0.2")
 }
 
